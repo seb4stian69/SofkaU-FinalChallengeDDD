3edebd87b7cb3c1c98eb58127a9e3feefb7e7846
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-08-19 14:49:40 -0500

    Añadiendo entidades al [AR]Almacen

--- Domain/src/main/java/co/com/sofka/ventas/almacen/entidades/Productos.java
+++ Domain/src/main/java/co/com/sofka/ventas/almacen/entidades/Productos.java
@@ -1,4 +1,32 @@
 package co.com.sofka.ventas.almacen.entidades;
 
-public class Productos {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.almacen.objetosvalor.PrecioProducto;
+import co.com.sofka.ventas.almacen.objetosvalor.ProductoID;
+import co.com.sofka.ventas.almacen.objetosvalor.TipoProducto;
+
+public class Productos extends Entity<ProductoID> {
+
+    private final ProductoID productoID;
+    private final TipoProducto  tipoProducto;
+    private final PrecioProducto precioProducto;
+
+    public Productos(ProductoID entityId, ProductoID productoID, TipoProducto tipoProducto, PrecioProducto precioProducto) {
+        super(entityId);
+        this.productoID = productoID;
+        this.tipoProducto = tipoProducto;
+        this.precioProducto = precioProducto;
+    }
+
+    public ProductoID getProductoID() {
+        return productoID;
+    }
+
+    public TipoProducto getTipoProducto() {
+        return tipoProducto;
+    }
+
+    public PrecioProducto getPrecioProducto() {
+        return precioProducto;
+    }
 }
--- Domain/src/main/java/co/com/sofka/ventas/almacen/entidades/Ubicacion.java
+++ Domain/src/main/java/co/com/sofka/ventas/almacen/entidades/Ubicacion.java
@@ -1,4 +1,32 @@
 package co.com.sofka.ventas.almacen.entidades;
 
-public class Ubicacion {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.almacen.objetosvalor.Ciudad;
+import co.com.sofka.ventas.almacen.objetosvalor.Direccion;
+import co.com.sofka.ventas.almacen.objetosvalor.UbicacionID;
+
+public class Ubicacion extends Entity<UbicacionID> {
+
+    private final UbicacionID ubicacionID;
+    private final Direccion direccion;
+    private final Ciudad ciudad;
+
+    public Ubicacion(UbicacionID entityId, UbicacionID ubicacionID, Direccion direccion, Ciudad ciudad) {
+        super(entityId);
+        this.ubicacionID = ubicacionID;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+    }
+
+    public UbicacionID getUbicacionID() {
+        return ubicacionID;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
 }
