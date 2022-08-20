e93ce652589c61eb23539ebd60a4b1291072a1f4
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-08-20 16:05:11 -0500

    Añadiendo entidades a [AR]Empleados

--- Domain/src/main/java/co/com/sofka/ventas/empleados/entidades/DatosPersonales.java
+++ Domain/src/main/java/co/com/sofka/ventas/empleados/entidades/DatosPersonales.java
@@ -1,4 +1,34 @@
 package co.com.sofka.ventas.empleados.entidades;
 
-public class DatosPersonales {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.clientes.objetosvalor.Nombre;
+import co.com.sofka.ventas.empleados.objetosvalor.DatosPersonalesID;
+import co.com.sofka.ventas.empleados.objetosvalor.MedioPago;
+import co.com.sofka.ventas.empleados.objetosvalor.Salario;
+
+public class DatosPersonales extends Entity<DatosPersonalesID> {
+
+    private final Nombre nombre;
+    private final MedioPago medioPago;
+    private final Salario salario;
+
+    public DatosPersonales(DatosPersonalesID entityId, Nombre nombre, MedioPago medioPago, Salario salario) {
+        super(entityId);
+        this.nombre = nombre;
+        this.medioPago = medioPago;
+        this.salario = salario;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public MedioPago getMedioPago() {
+        return medioPago;
+    }
+
+    public Salario getSalario() {
+        return salario;
+    }
+
 }
--- Domain/src/main/java/co/com/sofka/ventas/empleados/entidades/Labor.java
+++ Domain/src/main/java/co/com/sofka/ventas/empleados/entidades/Labor.java
@@ -1,4 +1,27 @@
 package co.com.sofka.ventas.empleados.entidades;
 
-public class Labor {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.empleados.objetosvalor.Horario;
+import co.com.sofka.ventas.empleados.objetosvalor.LaborID;
+import co.com.sofka.ventas.empleados.objetosvalor.Tipo;
+
+public class Labor extends Entity<LaborID> {
+
+    private final Tipo tipo;
+    private final Horario horario;
+
+    public Labor(LaborID entityId, Tipo tipo, Horario horario) {
+        super(entityId);
+        this.tipo = tipo;
+        this.horario = horario;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
 }
