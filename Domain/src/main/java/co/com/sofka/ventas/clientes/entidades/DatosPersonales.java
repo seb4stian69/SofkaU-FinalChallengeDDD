c9db48a33fc3968223683c293ba0f806b22d0e3a
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2022-08-20 09:26:17 -0500

    Añadiendo entidades al [AR]Cliente

--- Domain/src/main/java/co/com/sofka/ventas/clientes/entidades/DatosPersonales.java
+++ Domain/src/main/java/co/com/sofka/ventas/clientes/entidades/DatosPersonales.java
@@ -1,4 +1,26 @@
 package co.com.sofka.ventas.clientes.entidades;
 
-public class DatosPersonales {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.clientes.objetosvalor.DatosPersonalesID;
+import co.com.sofka.ventas.clientes.objetosvalor.Nombre;
+import co.com.sofka.ventas.clientes.objetosvalor.NumeroTelefono;
+
+public class DatosPersonales extends Entity<DatosPersonalesID> {
+
+    private final Nombre nombre;
+    private final NumeroTelefono numeroTelefono;
+
+    public DatosPersonales(DatosPersonalesID entityId, Nombre nombre, NumeroTelefono numeroTelefono) {
+        super(entityId);
+        this.nombre = nombre;
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public NumeroTelefono getNumeroTelefono() {
+        return numeroTelefono;
+    }
 }
--- Domain/src/main/java/co/com/sofka/ventas/clientes/entidades/TipoCliente.java
+++ Domain/src/main/java/co/com/sofka/ventas/clientes/entidades/TipoCliente.java
@@ -1,4 +1,20 @@
 package co.com.sofka.ventas.clientes.entidades;
 
-public class TipoCliente {
+import co.com.sofka.domain.generic.Entity;
+import co.com.sofka.ventas.clientes.objetosvalor.Tipo;
+import co.com.sofka.ventas.clientes.objetosvalor.TipoClienteID;
+
+public class TipoCliente extends Entity<TipoClienteID> {
+
+    private final Tipo tipo;
+
+    public TipoCliente(TipoClienteID entityId, Tipo tipo) {
+        super(entityId);
+        this.tipo = tipo;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
 }
